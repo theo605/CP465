e1898f0c431ddadc9f9bffad78f66b76cfa0fae2
jdkipfer <jdkipfer@gmail.com>
2020-04-02 19:45:39 -0400

    Fixed Algorithm, added cluster output to console
    Fixed Algorithm, added cluster output to console

--- CP465_KMeans/src/goos7850/Main.java
+++ CP465_KMeans/src/goos7850/Main.java
@@ -90,12 +90,12 @@ public class Main {
                         }
 			closest=0;
                         //System.out.print(clusters.size());
-			closestDist=clusters.get(0).distanceFromCenter(point);
+			closestDist=clusters.get(0).distanceFromCentroid(point);
 			//find closest cluster
 			for (int j=0;j<k;j++) {
-				if(clusters.get(j).distanceFromCenter(point)<closestDist) {
+				if(clusters.get(j).distanceFromCentroid(point)<closestDist) {
 					closest=j;
-					closestDist=clusters.get(0).distanceFromCenter(point);
+					closestDist=clusters.get(0).distanceFromCentroid(point);
 				}
 			}
 			clusters.get(closest).addPoint(point);
@@ -104,7 +104,7 @@ public class Main {
 		for (int i=0;i<iterations; i++){
                     //recalculate centers
                     for (int j=0;j<clusters.size(); j++){
-                        clusters.get(j).setCenter();
+                        clusters.get(j).setCentroid();
                         clusters.get(j).clearPoints();
                     }
                     //assign points
@@ -112,24 +112,31 @@ public class Main {
 			point = points.get(j);
 			closest=0;
                         //System.out.print(clusters.size());
-			closestDist=clusters.get(0).distanceFromCenter(point);
+			closestDist=clusters.get(0).distanceFromCentroid(point);
 			//find closest cluster
 			for (int m=0;m<k;m++) {
-				if(clusters.get(m).distanceFromCenter(point)<closestDist) {
+				if(clusters.get(m).distanceFromCentroid(point)<closestDist) {
 					closest=m;
-					closestDist=clusters.get(0).distanceFromCenter(point);
+					closestDist=clusters.get(0).distanceFromCentroid(point);
 				}
 			}
 			clusters.get(closest).addPoint(point);
 		}
                 }
-		System.out.println("Variance C1: "+clusters.get(0).getVariance());
-		System.out.println("Variance C2: "+clusters.get(1).getVariance());
+                
 		
 		JFrame f = new JFrame("Test");
 		f.setSize(1200,1000);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setContentPane(new GraphView(points, clusters, minX, maxX, minY, maxY));
 		f.setVisible(true);
+                
+                for (int i=0;i<k;i++) {
+                    System.out.println(i+"\n");
+                    System.out.println("Variance: "+clusters.get(i).getVariance());
+                    clusters.get(i).printPoints();
+			
+		}
+		
 	}
 }
